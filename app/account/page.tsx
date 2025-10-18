80334e402d46b88e5effaa51534ba17ffb913199
shijisan <cjsantos2411638@gmail.com>
2025-10-18 18:38:10 +0800

    fixed notes DELETE api to follow vercel deployment rules

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -17,15 +17,15 @@ export default function Account() {
                 <Card>
                     <CardContent>
                         <Image
-                            src={session?.user.image || "https://placehold.co/200/avif"}
+                            src={session?.user?.image || "https://placehold.co/200/avif"}
                             height={200}
                             width={200}
-                            alt={session?.user.name || "User"}
+                            alt={session?.user?.name || "User"}
                             className="size-32 rounded-full"
                         />
 
-                        <p>{session?.user.name}</p>
-                        <p>{session?.user.email}</p>
+                        <p>{session?.user?.name}</p>
+                        <p>{session?.user?.email}</p>
                         <CardAction className="flex-col flex gap-2 w-full mt-4" >
                             <Button
                                 variant="default"
